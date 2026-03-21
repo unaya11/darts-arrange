95a1861ae4f9881de8c6002f666886a213294f88
unaya11 <aaa@example.com>
2026-03-21 17:13:07 +0900

    add eslint and prettier

---
+++ eslint.config.js
@@ -0,0 +1,11 @@
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import prettier from 'eslint-config-prettier';
+
+export default tseslint.config(js.configs.recommended, ...tseslint.configs.recommended, prettier, {
+  rules: {
+    'no-unused-vars': 'warn',
+
+    '@typescript-eslint/no-unused-vars': ['warn'],
+  },
+});
