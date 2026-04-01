1debcffd32ef212285a0d5af3c1bd53689949a4d
unaya11 <aaa@example.com>
2026-04-02 00:33:13 +0900

    refactor: darts.tsのリファクタリング

--- src/constants/darts.ts
+++ src/constants/darts.ts
@@ -1,13 +1,10 @@
 export const numbers = Array.from({ length: 20 }, (_, i) => i + 1);
-export const bull = [25, 50];
+export const bull = [50, 25];
 export const leftNumbers = [...numbers.flatMap((num) => [num * 2]), 50];
 
 // 結果を降順に表示するため、reverseする
-// reverse()が元の配列を反転させるためslice()でコピーする
+// reverse()はtoReversed()は元の配列の中身を変更しない
 export const allNumbers = [
-  ...numbers
-    .slice()
-    .reverse()
-    .flatMap((num) => [num * 3, num * 2, num]),
+  ...numbers.toReversed().flatMap((num) => [num * 3, num * 2, num]),
   ...bull,
 ];
