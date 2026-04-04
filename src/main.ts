36fb266f9c406a74530be71abdac569a6f1bc338
unaya11 <aaa@example.com>
2026-04-04 21:08:23 +0900

    refactor: 1投目と3投目の処理、インポート部分、上がり目の変数名を修正

--- src/main.ts
+++ src/main.ts
@@ -1,33 +1,30 @@
 import './styles/main.css';
 import * as view from './ui/view';
 import * as darts from './constants/darts';
-import * as reader from './ui/dartsInputReader';
-import { calcScore } from './logic/score';
-import { addSingleNumberThrowList } from './logic/score';
-import { getInputNumber } from './ui/dartsInputReader';
-
+import { addSingleNumberThrowList, calcScore } from './logic/score';
+import { getInputNumber, getSelectNumbers } from './ui/dartsInputReader';
 view.checkAll?.addEventListener('click', () => {
   view.toggleAllChecks();
 });
 
 view.showButton?.addEventListener('click', () => {
   // 一投目が指定されている場合はその値とそのシングルを対象とし、指定されていない場合はすべてを対象とする
-  const selectFirstNumbers = reader.getSelectNumbers('input[type=checkbox]:checked.first-checks');
+  const selectFirstNumbers = getSelectNumbers('input[type=checkbox]:checked.first-checks');
   const firstThrowList = selectFirstNumbers
     ? addSingleNumberThrowList(selectFirstNumbers)
     : darts.allNumbers;
 
   // 三投目が指定されている場合はその値を、指定されていない場合はすべてのダブルを対象とする
-  const thirdThrowList =
-    reader.getSelectNumbers('input[type=checkbox]:checked.checks') ?? darts.leftNumbers;
+  const selectThirdNumbers = getSelectNumbers('input[type=checkbox]:checked.checks');
+  const thirdThrowList = selectThirdNumbers ?? darts.leftNumbers;
 
-  const leftNumber = getInputNumber();
-  if (leftNumber === undefined) {
+  const targetScore = getInputNumber();
+  if (targetScore === undefined) {
     view.nullView();
     return;
   }
-  const calcScoreList = calcScore(leftNumber, firstThrowList, thirdThrowList);
-  view.openDialog(leftNumber, calcScoreList);
+  const calcScoreList = calcScore(targetScore, firstThrowList, thirdThrowList);
+  view.openDialog(targetScore, calcScoreList);
 });
 
 view.closeButton?.addEventListener('click', () => {
